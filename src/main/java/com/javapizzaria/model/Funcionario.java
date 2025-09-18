4ae55c83ec8e1db6650666b7bff1c46afdf08d8b
joão victor <joao.nogueira@dcx.ufpb.br>
2025-09-18 09:43:01 -0300

    Update Funcionario.java

--- src/main/java/com/javapizzaria/model/Funcionario.java
+++ src/main/java/com/javapizzaria/model/Funcionario.java
@@ -3,21 +3,21 @@ package com.javapizzaria.model;
 public class Funcionario extends Usuario {
     private double salario;
     private String turno;
-    private Ponto ponto; // Adicionado
+    private Ponto ponto; 
 
     public Funcionario(String nome, double salario, String turno) {
         super(nome);
         this.salario = salario;
         this.turno = turno;
-        this.ponto = new Ponto(this); // Inicializa o ponto
+        this.ponto = new Ponto(this); 
     }
 
-    // Getters
+    
     public double getSalario() { return salario; }
     public String getTurno() { return turno; }
-    public Ponto getPonto() { return ponto; } // Getter do ponto
+    public Ponto getPonto() { return ponto; } 
 
-    // Setters
+    
     public void setSalario(double salario) { this.salario = salario; }
     public void setTurno(String turno) { this.turno = turno; }
 
